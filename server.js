8b03edb9feca2cbfb5c072f3a63f23a077deb428
unknown <jihadsheikh98781@gmail.com>
2026-08-20 10:51:02 +0600

    Prepare server for deployment

--- server.js
+++ server.js
@@ -6,12 +6,10 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Public folder
 app.use(express.static("public"));
 
-// Socket connection
 io.on("connection", (socket) => {
     console.log("A user connected:", socket.id);
 
@@ -20,6 +18,6 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
